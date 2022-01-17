663b9effee37b7385066ff24908e62a11ba150f5
Arun <arunsingh641997@gmail.com>
2022-01-17 12:53:52 +0530

    3rd commit

---
+++ IncAndDec.java
@@ -0,0 +1,23 @@
+package com.company.Java;
+
+public class IncAndDec {
+    public static void main(String[] args) {
+        //Increment operators a++,++a
+        //decrement operators a--,--a
+
+        // Incremnting and decrementing Java variable;
+        //Increment
+        int a = 45;
+        System.out.println(a++);//yaha pe pehle print ho rha hai then increment ho rha hai
+        int b = 44;
+        System.out.println(++b);// yaha pe pehle increment ho rha hai then print ho rha hai
+        //Decrement
+        int c = 45;
+        System.out.println(c--);// yaha pe pehle print ho rha hai then decrement ho rha hai
+        int d = 45;
+        System.out.println(--d);// yaha pe pehle decrement ho hai rha hai then print ho rha hai
+
+
+    }
+
+}
