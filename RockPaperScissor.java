611c84cfc8870e6e66e0bd7349055768f30d7793
Arun <arunsingh641997@gmail.com>
2022-01-20 18:02:03 +0530

    Score System Updated Not Fully

--- RockPaperScissor.java
+++ RockPaperScissor.java
@@ -9,7 +9,7 @@ public class RockPaperScissor {
         //Taking Name From User
         System.out.println("Enter Your Name");
         String name = s.nextLine();
-        System.out.println("We Welcome You "+name);
+        System.out.println("We Welcome You " + name);
         //Printing Instructions
         System.out.println("""
                 Instructions :\s
@@ -17,8 +17,8 @@ public class RockPaperScissor {
                  2.Press 2 for Paper\s
                  3.Press 3 for Scissor\s
                  If Tie Both will get One Point""");
-        int n=0;
-        while(n<5) {
+        int n = 0;
+        while (n < 5) {
 
             int var = s.nextInt();
             System.out.println(name + " Selected : ");
@@ -28,6 +28,7 @@ public class RockPaperScissor {
                 case 3 -> System.out.println("Scissor");
                 default -> System.out.println("Invalid Option");
             }
+
 
             // Creating Random Choice For Computer
             Random r = new Random();
@@ -41,30 +42,60 @@ public class RockPaperScissor {
             } else {
                 System.out.println("Scissor");
             }
+            String Result;
 
             if (random == 0 && var == 1 || random == 1 && var == 2 || random == 2 && var == 3) {
-                System.out.println("Tie");
+                Result = "Tie";
+                System.out.println(Result);
             } else if (random == 0 && var == 2) {
-                System.out.println(name + " Wins");
+                Result = name + " wins";
+                System.out.println(Result);
+
             } else if (random == 0 && var == 3) {
-                System.out.println("Computer Wins");
+                Result = "Computer Wins";
+                System.out.println(Result);
             } else if (random == 1 && var == 1) {
-                System.out.println("Computer Wins");
+                Result = "Computer Wins";
+                System.out.println(Result);
             } else if (random == 1 && var == 3) {
-                System.out.println(name + " Wins");
+                Result = name + " wins";
+                System.out.println(Result);
             } else if (random == 2 && var == 2) {
-                System.out.println("Computer Wins");
+                Result = "Computer Wins";
+                System.out.println(Result);
             } else if (random == 2 && var == 1) {
-                System.out.println(name + " Wins");
-            }
-            else{
-                System.out.println("Computer Wins");
+                Result = name + " wins";
+                System.out.println(Result);
+            } else {
+                Result = "Computer Wins";
+                System.out.println(Result);
             }
 
-            if(n<4){
+
+            //Score prediction
+            int i = 0;
+            int j = 0;
+
+            if (Result.equals("Tie")) {
+                i++;
+                j++;
+            } else if (Result.equals("Computer Wins")) {
+                j++;
+            } else if (Result.equals(name + " wins")) {
+                i++;
+            }
+
+            System.out.println(name + " score : " + i);
+            System.out.println("Computer score : " + j);
+
+            //Printing default Statement
+            if (n < 4) {
                 System.out.println("Make your Choice Again");
+
             }
             n++;
+
+
         }
 
 
