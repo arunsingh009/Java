7223926d290647d34419ff1a7e2a7906a9e6a890
Arun <arunsingh641997@gmail.com>
2022-01-20 19:16:39 +0530

    Printing reverse order of Natural Numbers

---
+++ printingReverseNaturalNumbers.java
@@ -0,0 +1,17 @@
+package com.company.Java;
+import java.util.Scanner;
+
+public class printingReverseNaturalNumbers {
+    public static void main(String[] args) {
+        System.out.println("We are printing Reverse natural number using for loop");
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter upper Limit");
+        int input = s.nextInt();
+        int i=0;
+        System.out.println("list of odd numbers");
+        for (i=input;i>=0;i--){
+            System.out.println(i);
+        }
+    }
+
+}
