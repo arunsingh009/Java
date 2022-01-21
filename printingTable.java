5a034f98bb086338791446ff9b5b5f82d7d03c5c
Arun <arunsingh641997@gmail.com>
2022-01-21 14:40:13 +0530

    Wap to print multiplication Table

--- printingTable.java
+++ printingTable.java
@@ -1,7 +1,18 @@
 package com.company.Java;
+import java.util.Scanner;
 
 public class printingTable {
     public static void main(String[] args) {
         System.out.println("We are Printing table of given number");
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter Number : ");
+        int input = s.nextInt();
+        int out;
+        int n = 1;
+        while(n<=10){
+            out = (input*n);
+            System.out.println(input+" * " +n+" = "+out);
+            n++;
+        }
     }
 }
